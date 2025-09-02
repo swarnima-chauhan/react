a5ab605fb18c35bd468e9e703a4c681012f96785
swarnima-chauhan <swarnimc130101@gmail.com>
2025-09-02 16:04:06 +0530

    6a done

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,33 +1,11 @@
-import "./checkout-header.css";
+import { ChechkoutHeader } from "./CheckoutHeader";
 import "./CheckoutPage.css";
-import { Link } from "react-router";
 
 export function CheckoutPage() {
   return (
     <>
       <title>Checkout</title>
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </Link>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <Link className="return-to-home-link" to="/">
-              3 items
-            </Link>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <ChechkoutHeader />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>

448d04712b05985a68918544d92a57fa2195404e
swarnima-chauhan <swarnimc130101@gmail.com>
2025-09-02 15:26:39 +0530

    moved checkout psage into react, update links to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,17 +1,17 @@
-import { HomePage } from './pages/HomePage'
-import {Routes, Route} from 'react-router'
-import './App.css'
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { Routes, Route } from "react-router";
+import "./App.css";
 
 function App() {
-
   return (
-   <>
-   <Routes>
-    <Route index element={<HomePage/>}/>
-    <Route path="checkout" element={<div>Test</div>}/>
-   </Routes>
-   </>
-  )
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;

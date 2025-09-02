832f1bb377b9acf43dc4e87b988088688054a4cf
swarnima-chauhan <swarnimc130101@gmail.com>
2025-09-02 15:38:33 +0530

    moved trackin page successfully

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { Routes, Route } from "react-router";
 import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </>
   );
